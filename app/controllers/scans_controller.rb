8fb4d181b3ad0c3dd722468dc4b72ac00963bd42
Forester Vosburgh <forester.vosburgh@rigor.com>
2016-08-12 16:58:33 -0400

    Upgrade rails and enforce strong params whitelisting

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -40,13 +40,31 @@ class ScansController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_scan
-      @scan = Scan.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_scan
+    @scan = Scan.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def scan_params
-      params.require(:scan).permit!
+  # Only allow a trusted parameter "white list" through.
+  def scan_params
+    params.require(:scan).permit(:event, :result, commit: permit_recursive_params(params[:scan][:commit]))
+  end
+
+  # Strong params currently does not permit hashes.
+  # This fix was found here https://gist.github.com/koenpunt/ac279e05cfeb0954ca763344fc0240b4#file-permit_recursive_params-rb
+  def permit_recursive_params(params)
+    (params.try(:to_unsafe_h) || params).map do |key, value|
+      if value.is_a?(Array)
+        if value.first.respond_to?(:map)
+          { key => [ permit_recursive_params(value.first) ] }
+        else
+          { key => [] }
+        end
+      elsif value.is_a?(Hash)
+        { key => permit_recursive_params(value) }
+      else
+        key
+      end
     end
+  end
 end
