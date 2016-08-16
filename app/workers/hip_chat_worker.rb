ad8769c2631f175b08d99bc7374c79d8ee4bd841
Forester Vosburgh <forester.vosburgh@rigor.com>
2016-08-16 15:24:20 -0400

    Missed renaming commit_info to commit

--- app/workers/hip_chat_worker.rb
+++ app/workers/hip_chat_worker.rb
@@ -5,7 +5,7 @@ class HipChatWorker
 
   def perform commit_info, score_result, color, comparison_url
     HipChatNotifier.new({
-                          commit_info: commit_info,
+                          commit: commit_info,
                           score_result: score_result,
                           color: color,
                           comparison_url: comparison_url
